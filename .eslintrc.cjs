aeddb700202344afc92c3a3bcfe7189024115ba8
Jacob Paris <jacob@jacobparis.com>
2023-09-02 23:15:10 +0300

    fix: update to remix v2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,3 @@
-const appFiles = ['app/**']
-
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: [
@@ -18,21 +16,4 @@ module.exports = {
     ],
     "import/no-duplicates": "warn",
   },
-  overrides: [
-		{
-			plugins: ['remix-react-routes'],
-			files: appFiles,
-			rules: {
-				'remix-react-routes/use-link-for-routes': 'error',
-				'remix-react-routes/require-valid-paths': 'error',
-				// disable this one because it doesn't appear to work with our
-				// route convention. Someone should dig deeper into this...
-				'remix-react-routes/no-relative-paths': [
-					'off',
-					{ allowLinksToSelf: true },
-				],
-				'remix-react-routes/no-urls': 'error',
-			},
-		},
-	],
 }
--- remix.config.js
+++ remix.config.js
@@ -1,15 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ignoredRouteFiles: ["**/.*"],
   tailwind: true,
   postcss: true,
-  serverModuleFormat: "esm",
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
 }
